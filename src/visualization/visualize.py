c5c727ea45855ad8a25caea22be9e2820ef8f07b
J.R. Powers-Luhn <floobyt@gmail.com>
2018-10-20 16:53:10 -0400

    updated picture with annotations

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -25,10 +25,19 @@ plt.savefig(os.path.join(plots_dir, "explained_variance.png"), dpi=300)
 plt.clf()
 
 # Plot of cumulative explained variance
-plt.plot(np.cumsum(pca.explained_variance_ratio_))
-plt.axhline(y=0.95, linestyle=":", color='r', alpha=0.5)
-plt.xlabel(r"Principal Components included in model, $k$")
-plt.ylabel("Cumulative Variance Explained")
+f, ax = plt.subplots()
+ax.plot(np.cumsum(pca.explained_variance_ratio_), '*-')
+ax.axhline(y=0.95, linestyle=":", color='r', alpha=0.5)
+ax.axvline(x=pca.to_explain_variance_frac(0.95), linestyle=":", color='r',
+            alpha=0.5)
+ax.set_xlabel(r"Principal Components included in model, $k$")
+ax.set_ylabel("Cumulative Variance Explained")
+ax.annotate(f"{pca.to_explain_variance_frac(0.95)} "
+            f"components required \nfor 95% variance", 
+            xy=(pca.to_explain_variance_frac(0.95), 0.95), xycoords='data',
+            arrowprops=dict(facecolor='black', shrink=0.05),
+            xytext=(0.75, 0.25), textcoords='axes fraction',
+            horizontalalignment="center")
 plt.savefig(os.path.join(plots_dir, 
                          "cumulative_explained_variance.png"), dpi=300)
 plt.clf()
