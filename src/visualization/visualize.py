4509e20511249f856f5b7ccfe8336818ae31be37
J.R. Powers-Luhn <floobyt@gmail.com>
2018-10-20 18:10:08 -0400

    Scatter plot of first 2 PC's

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -26,18 +26,41 @@ plt.clf()
 
 # Plot of cumulative explained variance
 f, ax = plt.subplots()
-ax.plot(np.cumsum(pca.explained_variance_ratio_), '*-')
+ax.plot(range(1, pca.explained_variance_.shape[0]+1),
+        np.cumsum(pca.explained_variance_ratio_), '*-')
 ax.axhline(y=0.95, linestyle=":", color='r', alpha=0.5)
-ax.axvline(x=pca.to_explain_variance_frac(0.95), linestyle=":", color='r',
+ax.axvline(x=pca.to_explain_variance_frac(0.95)+1, linestyle=":", color='r',
             alpha=0.5)
 ax.set_xlabel(r"Principal Components included in model, $k$")
 ax.set_ylabel("Cumulative Variance Explained")
-ax.annotate(f"{pca.to_explain_variance_frac(0.95)} "
-            f"components required \nfor 95% variance", 
-            xy=(pca.to_explain_variance_frac(0.95), 0.95), xycoords='data',
+ax.annotate(f"{pca.to_explain_variance_frac(0.95) + 1} "
+            f"components required \nfor $\geq$95% variance", 
+            xy=(pca.to_explain_variance_frac(0.95)+1, 0.95), xycoords='data',
             arrowprops=dict(facecolor='black', shrink=0.05),
             xytext=(0.75, 0.25), textcoords='axes fraction',
             horizontalalignment="center")
 plt.savefig(os.path.join(plots_dir, 
                          "cumulative_explained_variance.png"), dpi=300)
+plt.clf()
+
+# Scatter plot of the PC's against each other
+pca2 = PCA(variance=0.95).fit(df_num.values)
+df_pca = pd.DataFrame(pca2.transform(df_num.values))
+df_pca['School'] = df["Name"]
+
+# Code to label points obtained from 
+# https://stackoverflow.com/questions/46027653/adding-labels-in-x-y-scatter-plot-with-seaborn
+def label_point(x, y, val, ax):
+    a = pd.concat({'x': x, 'y': y, 'val': val}, axis=1)
+    for i, point in a.iterrows():
+        ax.text(point['x']+.02, point['y'], str(point['val']), 
+                fontdict={'size': 4})
+
+f = plt.figure(figsize=(20,20))
+with sns.axes_style("darkgrid"):
+    ax = sns.relplot(x=0, y=1, data=df_pca)
+    ax.set_xlabels("Principal component 0")
+    ax.set_ylabels("Principal Component 1")
+    label_point(df_pca[0], df_pca[1], df_pca.School, plt.gca())
+plt.savefig(os.path.join(plots_dir, "pc1_v_pc2.png"), dpi=300)
 plt.clf()
